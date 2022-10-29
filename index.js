f28f17e430722a60caf86106c20b76ffdef209fc
Kamrul Hasan <CR7.kamrul.9090@gmail.com>
2022-10-29 18:27:02 +0600

    complete

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const app = express();
 const port = process.env.PORT || 5000;
 app.use(cors());
 
-const catagories = require('./data/catagories.json')
+const catagories = require('./data/catagories.json');
+const news = require('./data/news.json');
 
 app.get('/', (req, res) => {
     res.send('News API Running');
@@ -13,6 +14,33 @@ app.get('/', (req, res) => {
 app.get('/news-catagories', (req, res) => {
     res.send(catagories)
 })
+
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedNews = news.find(n => n._id === id)
+    res.send(selectedNews);
+})
+
+app.get('/news', (req, res) => {
+    res.send(news)
+})
+
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    if (id === '08') {
+        res.send(news)
+    }
+    else {
+        const categoryNews = news.filter(n => n.category_id === id);
+        res.send(categoryNews);
+    }
+})
+
+// app.get('/')
+
+
+
+
 
 app.listen(port, () => {
     console.log('Dragon News server running on port', port);
